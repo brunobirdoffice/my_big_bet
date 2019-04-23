61868ec5038540d632b94db862e7462263cc6319
brunobirdoffice <bruno@bird-office.com>
2019-04-23 16:38:16 +0200

    [API] add .gitignore

--- api/app.js
+++ api/app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const mongoose = require('mongoose')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const tokenApiFootBall = '8be74ba2332c4a0992846401a58aaaea'
 
-var app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/mbb', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', () => {
+    console.log('mongoDB error...')
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users.router')
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const app = express()
 
-module.exports = app;
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+module.exports = app
