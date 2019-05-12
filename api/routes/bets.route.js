8ce214272a041b48f01aa44ec90269f3a8e86933
brunobirdoffice <bruno@bird-office.com>
2019-05-12 18:56:39 +0200

    [API] update bet router

--- api/routes/bets.route.js
+++ api/routes/bets.route.js
@@ -2,16 +2,16 @@ const express = require('express')
 const betController = require('../controller/bet.controller')
 const router = express.Router()
 
-/* GET Bet . */
+/* GET Bet */
 router.get('/:id', betController.getBet)
 
-/* GET all Bets . */
+/* GET all Bets */
 router.get('', betController.getAllBetByUser)
 
-/* Create Bet . */
+/* Create Bet */
 router.post('/post', betController.createBet)
 
-/* Put Bet . */
+/* Put Bet */
 router.put('/:id', betController.updateBet)
 
 module.exports = router
